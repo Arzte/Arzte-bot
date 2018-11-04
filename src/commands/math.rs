ff90251d5b51dc1f6ccdb61fa7185ee90ac147f0
Faey Hoffmeyer <hoffmeyer25@gmail.com>
2018-11-03 20:01:48 -0500

    Initial bot structure
    Borrowed with modifications from https://github.com/serenity-rs/serenity/tree/current/examples/07_sample_bot_structure
    
    Instead of using env for discord bot token, config-rs is used to read a config file with the bot token.

---
+++ src/commands/math.rs
@@ -0,0 +1,8 @@
+command!(multiply(_ctx, msg, args) {
+    let one = args.single::<f64>().unwrap();
+    let two = args.single::<f64>().unwrap();
+
+    let product = one * two;
+
+    let _ = msg.channel_id.say(product);
+});
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+#[macro_use] extern crate log;
+#[macro_use] extern crate serenity;
+extern crate config;
+extern crate chrono;
+extern crate env_logger;
+extern crate kankyo;
+extern crate rand;
+
+mod commands;
+
+use serenity::framework::StandardFramework;
+use serenity::model::event::ResumedEvent;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use serenity::http;
+use std::collections::HashSet;
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+fn main() {
+    // Initialize the logger to use environment variables.
+    //
+    // In this case, a good default is setting the environment variable
+    // `RUST_LOG` to debug`.
+    env_logger::init().expect("Failed to initialize env_logger");
+
+    // Settings file, used currently for token
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name("Settings")).expect("No file called Settings.toml in same folder as bot");
+    // set settings to variables that can be accessed.
+    let token = settings.get_str("token").expect("No token/token value set in Settings file");
+
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
 fn main() {
     println!("Hello, world!");
+
+    let owners = match http::get_current_application_info() {
+        Ok(info) => {
+            let mut set = HashSet::new();
+            set.insert(info.owner.id);
+
+            set
+        },
+        Err(why) => panic!("Couldn't get application info: {:?}", why),
+    };
+
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c
+            .owners(owners)
+            .prefix("!"))
+        .command("ping", |c| c.cmd(commands::meta::ping))
+        .command("multiply", |c| c.cmd(commands::math::multiply))
+        .command("quit", |c| c
+            .cmd(commands::owner::quit)
+            .owners_only(true)));
+
+    if let Err(why) = client.start() {
+        error!("Client error: {:?}", why);
+    }
 }

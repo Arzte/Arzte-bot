e44363f03eaf45fa98c3cfc7f2eed1620fb32f3c
Faey Hoffmeyer <hoffmeyer25@gmail.com>
2018-11-04 15:25:54 -0500

    Use shard manger to shutdown bot

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -1,5 +1,22 @@
-command!(quit(ctx, msg, _args) {
-    ctx.quit();
+use super::super::ShardManagerContainer;
 
-    let _ = msg.reply("Shutting down!");
+command!(quit(ctx, msg, _args) {
+    // The shard manager is an interface for mutating, stopping, restarting, and
+    // retrieving information about shards.
+    let data = ctx.data.lock();
+
+    let shard_manager = match data.get::<ShardManagerContainer>() {
+        Some(v) => v,
+        None => {
+            let _ = msg.reply("There was a problem getting the shard manager");
+
+            return Ok(());
+        },
+    };
+
+    let mut manager = shard_manager.lock();
+
+    msg.reply("Shutting down!")?;
+
+    manager.shutdown_all();
 });

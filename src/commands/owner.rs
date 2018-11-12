28a03e4dd7c1451c98512699eaab2da9ee70e43f
Faey Hoffmeyer <hoffmeyer25@gmail.com>
2018-11-12 00:32:49 -0500

    Add initial git self-updating

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -21,9 +21,19 @@ command!(quit(ctx, msg, _args) {
     manager.shutdown_all();
 });
 
+use std::process::Command;
+
 command!(update(ctx, msg, _args) {
     msg.channel_id.broadcast_typing()?;
     if let Ok(mut message) = msg.channel_id.say("Now updating Arzte's Cute Bot, please wait....") {
+
+        let output = Command::new("git")
+            .args(&["pull", "-ff"])
+            .output()
+            .expect("failed to execute process");
+
+        message.edit(|m| m.content(format!("Now updating Arzte's Cut Bot, please wait....\n**```\n{}\n```**", String::from_utf8_lossy(&output.stdout))))?;
+
         // The shard manager is an interface for mutating, stopping, restarting, and
         // retrieving information about shards.
         let data = ctx.data.lock();

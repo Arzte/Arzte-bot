899a65598bd4654aeb3e342911152846befc664c
Faey Hoffmeyer <hoffmeyer25@gmail.com>
2018-11-14 02:42:03 -0500

    add built_info for convenient pkg info from when the bot was built
    Also begin partial restructure to ensure helper functions are separate from commands.

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,6 +1,8 @@
+use core::built_info;
+
 command!(about(_ctx, msg, _args) {
-    msg.channel_id.say("This is a small test-bot, developed by <@77812253511913472>, with help from serenity and it's resources.\n\n\
-    There are currently no set plans for this bot")?;
+    msg.channel_id.say(format!("{} (v ``{}``) is a small utility bot, developed by <@77812253511913472>, with help from serenity and it's resources.\n\n\
+    There are currently no set plans for this bot", built_info::PKG_NAME, built_info::PKG_VERSION))?;
 });
 
 use serenity::model::id::GuildId;
---
+++ src/core/mod.rs
@@ -0,0 +1,2 @@
+pub mod built_info;
+pub mod structs;

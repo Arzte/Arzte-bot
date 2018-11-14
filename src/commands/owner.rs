48307a51ff2f54a5d778ae7dde9d590d6595cd15
Faey Hoffmeyer <hoffmeyer25@gmail.com>
2018-11-14 18:14:51 -0500

    Move the long running update code to a new thread.

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -24,6 +24,7 @@ command!(quit(ctx, msg, _args) {
 use core::built_info;
 use core::structs::Github;
 use std::process::Command;
+use std::thread;
 
 command!(update(ctx, msg, _args) {
     msg.channel_id.broadcast_typing()?;
@@ -36,55 +37,56 @@ command!(update(ctx, msg, _args) {
         if local_short == github_short {
             msg.channel_id.say(format!("There are no updates available, perhaps you forgot to push to Github?\nlocal: ``{}`` matchs github:``{}``", local_short, github_short))?;
             return Ok(())
-        } else {
-            msg.channel_id.say(format!("There is a update available! :)\nHowever for testing I'm only outputting the values of the short commit I was built on, and GitHub. :(\nlocal: ``{}`` matches github: ``{}``", local_short, github_short))?;
         }
     };
 
-    if let Ok(mut message) = msg.channel_id.say("Now updating Arzte's Cute Bot, please wait....") {
+    thread::spawn(move || {
+            if let Ok(mut message) = msg.channel_id.say("Now updating Arzte's Cute Bot, please wait....") {
 
-        if let Ok(mut cmd_output) = msg.channel_id.say("**```\n \n```**") {
-            message.edit(|m| m.content("Pulling in the latest changes from github...."))?;
+                if let Ok(mut cmd_output) = msg.channel_id.say("**```\n \n```**") {
+                    message.edit(|m| m.content("Pulling in the latest changes from github...."))?;
 
-            let output = Command::new("git")
-                .args(&["pull", "-ff"])
-                .output()?;
+                    let output = Command::new("git")
+                        .args(&["pull", "-ff"])
+                        .output()?;
 
-            cmd_output.edit(|m| m.content(format!("**```\n{}\n```**", String::from_utf8_lossy(&output.stdout))))?;
-            message.edit(|m| m.content("Finished pulling updates from Github."))?;
+                    cmd_output.edit(|m| m.content(format!("**```\n{}\n```**", String::from_utf8_lossy(&output.stdout))))?;
+                    message.edit(|m| m.content("Finished pulling updates from Github."))?;
 
-            std::thread::sleep(std::time::Duration::from_millis(1000));
+                    std::thread::sleep(std::time::Duration::from_millis(1000));
 
-            message.edit(|m| m.content("Now compiling changes.... (This takes a long time)"))?;
-            let output2 = Command::new("/home/faey/.cargo/bin/cargo")
-                .args(&["+stable", "build", "--release"])
-                .current_dir("/home/faey/bot")
-                .output()?;
+                    message.edit(|m| m.content("Now compiling changes.... (This takes a long time)"))?;
+                    let output2 = Command::new("/home/faey/.cargo/bin/cargo")
+                        .args(&["+stable", "build", "--release"])
+                        .current_dir("/home/faey/bot")
+                        .output()?;
 
-            cmd_output.edit(|m| m.content(format!("**```\n{}\n```**", String::from_utf8_lossy(&output2.stderr))))?;
-            message.edit(|m| m.content("Finished compiling new changes."))?;
-        }
+                    cmd_output.edit(|m| m.content(format!("**```\n{}\n```**", String::from_utf8_lossy(&output2.stderr))))?;
+                    message.edit(|m| m.content("Finished compiling new changes."))?;
+                }
 
-    }
+            }
 
-    if let Ok(mut shard) = msg.channel_id.say("Getting shard manager, then telling the bot to shutdown...") {
-        // The shard manager is an interface for mutating, stopping, restarting, and
-        // retrieving information about shards.
-        let data = ctx.data.lock();
+            if let Ok(mut shard) = msg.channel_id.say("Getting shard manager, then telling the bot to shutdown...") {
+                // The shard manager is an interface for mutating, stopping, restarting, and
+                // retrieving information about shards.
+                let data = ctx.data.lock();
 
-        let shard_manager = match data.get::<ShardManagerContainer>() {
-            Some(v) => v,
-            None => {
-                let _ = shard.edit(|m| m.content("There was a problem getting the shard manager"));
+                let shard_manager = match data.get::<ShardManagerContainer>() {
+                    Some(v) => v,
+                    None => {
+                        let _ = shard.edit(|m| m.content("There was a problem getting the shard manager"));
 
-                return Ok(());
-            },
-        };
+                        return Ok(())
+                    },
+                };
 
-        let mut manager = shard_manager.lock();
+                let mut manager = shard_manager.lock();
 
-        shard.edit(|m| m.content("Updated! Restarting now!"))?;
+                shard.edit(|m| m.content("Updated! Restarting now!"))?;
 
-        manager.shutdown_all();
-    }
+                manager.shutdown_all();
+            }
+            Ok(())
+    });
 });

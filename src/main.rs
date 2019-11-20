620dd4b7f7ce7a8ca1790fb2e4bf639b1c54c44e
Iris Faey Hoffmeyer <hoffmeyer25@gmail.com>
2019-11-20 10:13:05 -0500

    Remove commented macro use's
    macro use while using cargo is a thing of the past, thank goodness

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 #![recursion_limit = "128"]
 #![allow(proc_macro_derive_resolution_fallback)]
 
-// #[macro_use]
-// extern crate log;
-// #[macro_use]
-// extern crate serenity;
-// #[macro_use]
-// extern crate serde_derive;
-// #[macro_use]
-// extern crate asciimath;
-// //#[macro_use]
-// // The above macro_use is commented out as it is currently not used, but will be in the future.
-// extern crate failure;
-
 mod commands;
 pub mod core;
 

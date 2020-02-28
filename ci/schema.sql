fe40390c60773c5b76b28cfd643ab89b694b44d3
Iris Faey Hoffmeyer <hoffmeyer25@gmail.com>
2020-02-28 13:22:17 -0500

    CI

---
+++ ci/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS guild (
+    id bigint PRIMARY KEY NOT NULL,
+    /* TODO make varchar(100) instead of text */
+    name text NOT NULL,
+    updated_at timestamptz,
+    prefix text NOT NULL DEFAULT 'a.'
+);
+

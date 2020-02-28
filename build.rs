bb44a3e6f27c073e3b3cacb544348082b40a56ca
Iris Faey Hoffmeyer <hoffmeyer25@gmail.com>
2020-02-28 10:26:47 -0500

    package less buildtime info into bot

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
 extern crate built;
 fn main() {
-    built::write_built_file().expect("Failed to acquire build-time information");
+    let mut options_default = built::Options::default();
+    let options = options_default
+        .set_compiler(false)
+        .set_ci(false)
+        .set_features(false)
+        .set_cfg(false);
+    let src = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dst = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("built.rs");
+    built::write_built_file_with_opts(&options, src.as_ref(), &dst)
+        .expect("Failed to acquire build-time information");
 }
